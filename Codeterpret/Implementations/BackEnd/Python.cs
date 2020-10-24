0381317b53a768190bd9139bc4386fd5e97ac5a7
Corey Lasley <cclas78@gmail.com>
2020-10-23 22:53:40 -0400

    Adding empty classes for future support

---
+++ Codeterpret/Implementations/BackEnd/Python.cs
@@ -0,0 +1,11 @@
+﻿using Codeterpret.Implementations.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codeterpret.Implementations.BackEnd
+{
+    public class Python : BackEndCodeBase
+    {
+    }
+}
